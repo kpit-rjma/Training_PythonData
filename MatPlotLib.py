09894e653cd615f106b3e249ef162db6521ea9b1
Rachel Ashburn <rachela@kpit.com>
2018-03-12 15:42:00 -0400

    Overhauled interaction

--- MatPlotLib.py
+++ MatPlotLib.py
@@ -2,11 +2,17 @@
 # The enviornment is the problem to set up, used Anaconda and the Conda commands to set up
 # Data taken from a phone using AndroSensor
 import numpy
-import matplotlib.pyplot as mpl
+import matplotlib.pyplot as plt
 import csv
 import sys
+import traceback
 
+# Change this to point to your specific AndroSensor CSV file
 filelocation = "C:\\Users\\rachela\\Documents\\phonedata.csv"
+
+# These are variables I want easily accessible to edit, so I'm storing them at the top
+optionsstring = "1 - Run Test Plot\n2 - Exit"
+exitoption = "2"
 
 # Get the data
 # Data is as follows:
@@ -32,14 +38,46 @@ filelocation = "C:\\Users\\rachela\\Documents\\phonedata.csv"
 # Also see: https://docs.scipy.org/doc/numpy/reference/generated/numpy.genfromtxt.html
 nameslist = ["AccelX", "AccelY", "AccelZ", "GravX", "GravY", "GravZ", "LAccelX", "LAccelY", "LAccelZ", "GyroX", "GyroY", "GyroZ", "Light", "MagX", "MagY", "MagZ", "OrienX", "OrienY", "OrienZ", "Proximity", "Pressure", "Sound", "Latitude", "Longitude", "Altitude", "GoogleAlt", "GoogleATM", "Speed", "Accuracy", "Orientation", "SatelliteCount", "Duration", "Date"]
 data = numpy.genfromtxt(filelocation, delimiter=',', names=nameslist, skip_header=1)
-# print(data) # Comment out when testing
+# print(data) # Comment out when NOT testing data grabbing
 
-print("When working with MatPlotLib, you'll want to ensure you're printing all the graphics you want to show.");
-print("In this case, we're going to want to display multiple plots in the console in succession based on the data gathered from the phone")
-print("Be aware that in the code, subplot() is called to create a single plot with the specified style and parameters.")
+# The following block of code (commented out) prints a test plot for you to check.
+# If this does not plot per expectations, then there might be an issue with your Numpy or MatPlotLib installations
+# If you run into issues, uncommenting this block out and attempting to use it is worthwhile.
+def runtestplot():
+    try:
+        n = 20
+        x = numpy.linspace(0, numpy.pi, n)
+        y = numpy.sin(x)
+        plt.plot(x, y)
+        plt.show()
+    except:
+        print("Something went wrong with printing the test plot...")
+        print("Please exit and check your Numpy and MatPlotLib installations.")
+        print("To install either from the command line (using pip), run the following:")
+        print("pip install numpy")
+        print("pip install matplotlib")
+        # The next line is for debugging only, leave commented out for delivery
+        #print(traceback.format_exc())
+    print("Test plot attempt complete.")
 
 # Print Light values over Time
-subpl1 = mpl.subplot()
-subpl1.plot((data["Duration"]/1000), data["Light"])
-subpl1.set_xlabel("Duration (Seconds)")
-subpl1.set_ylabel("Lumens")
+#subpl1 = plt.subplot()
+#subpl1.plot((data["Duration"]/1000), data["Light"])
+#subpl1.set_xlabel("Duration (Seconds)")
+#subpl1.set_ylabel("Lumens")
+
+# The following block of code ensures the user is able to navigate commands and controls the
+# movement from call to call of various subroutines.
+selection = ""
+while (selection.lower() != "exit" or selection.lower() != "e" or selection != exitoption):
+    print("Please type a command from the below list:")
+    print("(Remember to exit graphs before returning to the console window!)")
+    print(optionsstring)
+    selection = input("Type command: ")
+
+    if (selection == "1" or selection.lower() == "run test plot" or selection.lower() == "run test"):
+        runtestplot()
+    elif (selection.lower() != "exit" or selection.lower() != "e" or selection != exitoption):
+        break
+    else:
+        selection = input("\nCommand not recognized! Please try again...\n")
--- README.md
+++ README.md
@@ -4,6 +4,8 @@ The purpose of the code is not to necessarily create a final project but to lear
 The packages used insofar include Numpy and MatPlotLib.
 
 ## Requirements
+* Numpy, install from a prompt using: pip install numpy
+* MatPlotLib, install from a prompt using: pip install matplotlib
 * Data file from AndroSense--an Android app for tracking sensor data. This should be a CSV file.
 * The AndroSense file should be located somewhere on the computer you are running the script from.
 * Obtain the absolute path of this file, minding to replace "\" with "\\". You will need it in the Quick-Start.
@@ -11,7 +13,7 @@ The packages used insofar include Numpy and MatPlotLib.
 ## Quick-Start
 * Enter code of MatPlotLib.py and change the file location to your own AndroSense data file.
 * With Python installed and the PATH set up... run the following in the command line (ensure the current working directory contains the script):
-Python MatPlotLib.py
+python MatPlotLib.py
 * Graphics should display, or follow the prompts to view data.
 
 ## Key Notes
